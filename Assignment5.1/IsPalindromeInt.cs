b1604fbd2d4eef44a670dc80eac089c6d9c4b0e9
Jnglct <rogerscole99@gmail.com>
2025-08-04 15:40:45 -0500

    Add project files.

---
+++ Assignment5.1/IsPalindromeInt.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment5._1
+{
+    internal class IsPalindromeInt
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a integer");
+
+        input:
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                goto input;
+            }
+            int palNumb = 0;
+            int checkNumb = number;
+            if (number < 0) //if negative return false
+            {
+                Console.WriteLine("false");
+            }
+            else 
+            {
+                while (true)
+                {
+                    palNumb += number % 10;
+                    number /= 10;
+                    number = (int)number;
+                    if (number < 1)
+                    {
+                        break;
+                    }
+                    palNumb *= 10;
+                }
+                if (palNumb == checkNumb)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
